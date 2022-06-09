fcd048313d27db63046bce3ff0abf11de0debd34
clemleet <98111008+clemleet@users.noreply.github.com>
2022-06-09 18:11:43 +0300

    fix

--- instr.py
+++ instr.py
@@ -1,107 +1,41 @@
-#final_win.py
-from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
-from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont # проверка типов вводимых значений
-from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, QGridLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
-        
-from instr import *
+# instr.py
+from PyQt5.QtCore import QTime
 
-class FinalWin(QWidget):
-    def __init__(self, exp):
-        ''' окно, в котором проводится опрос '''
-        super().__init__()
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
 
-        #получаем данные об эксперименте
-        self.exp = exp
-
-        # создаём и настраиваем графические элелементы:
-        self.initUI()
-
-        #устанавливает связи между элементами
-        # self.connects()
-
-        #устанавливает, как будет выглядеть окно (надпись, размер, место)
-        self.set_appear()
-        
-        # старт:
-        self.show()
-
-    def results(self):
-        if self.exp.person.age < 7:
-            self.index = 0
-            return "нет данных для такого возраста"
-        self.index = (4 * (int(self.exp.test1) + int(self.exp.test2) + int(self.exp.test3)) - 200) / 10
-        if self.exp.person.age == 7 or self.exp.person.age == 8:
-            if self.index >= 21:
-                return txt_res1
-            elif self.index < 21 and self.index >= 17:
-                return txt_res2
-            elif self.index < 17 and self.index >= 12:
-                return txt_res3
-            elif self.index < 12 and self.index >= 6.5:
-                return txt_res4
-            else:
-                return txt_res5
-        if self.exp.person.age == 9 or self.exp.person.age == 10:
-            if self.index >= 19.5:
-                return txt_res1
-            elif self.index < 19.5 and self.index >= 15.5:
-                return txt_res2
-            elif self.index < 15.5 and self.index >= 10.5:
-                return txt_res3
-            elif self.index < 10.5 and self.index >= 5:
-                return txt_res4
-            else:
-                return txt_res5
-        if self.exp.person.age == 11 or self.exp.person.age == 12:
-            if self.index >= 18:
-                return txt_res1
-            elif self.index < 18 and self.index >= 14:
-                return txt_res2
-            elif self.index < 14 and self.index >= 9:
-                return txt_res3
-            elif self.index < 9 and self.index >= 3.5:
-                return txt_res4
-            else:
-                return txt_res5
-        if self.exp.person.age == 13 or self.exp.person.age == 14:
-            if self.index >= 16.5:
-                return txt_res1
-            elif self.index < 16.5 and self.index >= 12.5:
-                return txt_res2
-            elif self.index < 12.5 and self.index >= 7.5:
-                return txt_res3
-            elif self.index < 7.5 and self.index >= 2:
-                return txt_res4
-            else:
-                return txt_res5
-        if self.exp.person.age >= 15:
-            if self.index >= 15:
-                return txt_res1
-            elif self.index < 15 and self.index >= 11:
-                return txt_res2
-            elif self.index < 11 and self.index >= 6:
-                return txt_res3
-            elif self.index < 6 and self.index >= 0.5:
-                return txt_res4
-            else:
-                return txt_res5
-
-    def initUI(self):
-        ''' создает графические элементы '''
-        self.workh_text = QLabel(txt_workheart + self.results())
-        self.index_text = QLabel(txt_index + str(self.index))
-
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
-        self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)         
-        self.setLayout(self.layout_line)
-
-    ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
-    def set_appear(self):
-        self.setWindowTitle(txt_finalwin)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
+txt_hello = 'Добро пожаловать в программу по определению состояния здоровья!'
+txt_next = 'Начать'
+txt_instruction = ('Данное приложение позволит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.\n'
+                    'Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособности сердца при физической нагрузке.\n'
+                    'У испытуемого, находящегося в положении лежа на спине в течение 5 мин, определяют частоту пульса за 15 секунд;\n'
+                    'затем в течение 45 секунд испытуемый выполняет 30 приседаний.\n'
+                    'После окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсаций за первые 15 секунд,\n'
+                    'а потом — за последние 15 секунд первой минуты периода восстановления.\n'
+                    'Важно! Если в процессе проведения испытания вы почувствуете себя плохо (появится головокружение, шум в\n'
+                    'ушах, сильная одышка и др.), то тест необходимо прервать и обратиться к врачу.' )
+txt_title = 'Здоровье'
+txt_name = 'Введите Ф.И.О.:'
+txt_hintname = "Ф.И.О."
+txt_hintage = "0"
+txt_test1 = 'Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку "Начать первый тест", чтобы запустить таймер.\nРезультат запишите в соответствующее поле.'
+txt_test2 = 'Выполните 30 приседаний за 45 секунд. Для этого нажмите кнопку "Начать делать приседания",\nчтобы запустить счетчик приседаний.'
+txt_test3 = 'Лягте на спину и замерьте пульс сначала за первые 15 секунд минуты, затем за последние 15 секунд.\nНажмите кнопку "Начать финальный тест", чтобы запустить таймер.\nЗеленым обозначены секунды, в течение которых необходимо \nпроводить измерения, черным - минуты без замера пульсаций. Результаты запишите в соответствующие поля.'
+txt_sendresults = 'Отправить результаты'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Начать первый тест'
+txt_starttest2 = 'Начать делать приседания'
+txt_starttest3 = 'Начать финальный тест'
+time = QTime(0, 0, 15)
+txt_timer = time.toString("hh:mm:ss")
+txt_age = 'Полных лет:'
+txt_finalwin = 'Результаты'
+txt_index = 'Индекс Руфье: '
+txt_workheart = 'Работоспособность сердца: '
+txt_res1 = "низкая. Срочно обратитесь к врачу!"
+txt_res2 = "удовлетворительная. Обратитесь к врачу!"
+txt_res3 = "средняя. Возможно, стоит дополнительно обследоваться у врача."
+txt_res4 = "выше среднего"
+txt_res5 = "высокая"
